aef8a472a277957ca5468f422cf78edbd1c2deae
hoangquoc03 <h.oangquoc180305@gmail.com>
2025-05-17 07:52:08 +0000

    room

--- rentalRooms/src/App.java
+++ rentalRooms/src/App.java
@@ -2,6 +2,8 @@
 
 
 import test.testrentalroom;
+//import test.testRoom;
+import models.room;
 
 
 
@@ -9,5 +11,8 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         testrentalroom.rentalroom();
+       
+        room.hienthiphong();
+
     }
 }
