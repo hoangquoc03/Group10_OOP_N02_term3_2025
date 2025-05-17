91d9837275b2dfa2cd600d382ae9ce0cc9574785
hoangquoc03 <h.oangquoc180305@gmail.com>
2025-05-17 07:35:49 +0000

    initial project

---
+++ rentalRooms/src/App.java
@@ -0,0 +1,13 @@
+
+
+
+import test.testrentalroom;
+
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+        testrentalroom.rentalroom();
+    }
+}
