cb5930a6357e1caea847a60a2625ef676d2e40cb
LeQuangThang205 <tq10022005@gmail.com>
2025-06-07 08:18:49 +0000

    update3

--- rentalRooms/src/CRUD/Output.java
+++ rentalRooms/src/CRUD/Output.java
@@ -5,13 +5,15 @@ import models.CrudManager;
 
 public class Output {
     public static void hienThiBangPhong(CrudManager<Room> roomManager) {
-        System.out.printf("%-10s %-10s %-20s %-15s %-15s %-12s %-20s %-15s %-12s\n",
-            "Room ID", "Price", "Landlord Name", "Phone", "Address", "Tenant ID ","Tenant Name", "Phone", "Ngày thuê");
+        System.out.printf("%-10s %-10s %-20s %-15s %-20s %-15s %-12s %-20s %-15s %-12s\n",
+            "Room ID","Room Address", "Price", "Landlord Name", "Phone", "Address", "Tenant ID", "Tenant Name", "Phone", "Ngày thuê");
+
         System.out.println("----------------------------------------------------------------------------------------------------");
 
         for (Room room : roomManager.getItems()) {
-            System.out.printf("%-10s %-10.2f %-20s %-15s %-15s %-12s %-20s %-15s %-12s\n",
+            System.out.printf("%-10s %-10s %-10.2f %-20s %-15s %-15s %-12s %-20s %-15s %-12s\n",
                 room.getID(),
+                room.getAddress(),
                 room.getPrice(),
                 room.getLandlord().getName(),
                 room.getLandlord().getSdt(),
