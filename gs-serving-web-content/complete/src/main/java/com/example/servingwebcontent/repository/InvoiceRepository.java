92ba35f0cb86180a9a3114e481c02c158d6981bd
hoangquoc03 <h.oangquoc180305@gmail.com>
2025-06-27 11:43:10 +0000

    Pending changes exported from your codespace

--- gs-serving-web-content/complete/src/main/java/com/example/servingwebcontent/repository/InvoiceRepository.java
+++ gs-serving-web-content/complete/src/main/java/com/example/servingwebcontent/repository/InvoiceRepository.java
@@ -2,9 +2,12 @@ package com.example.servingwebcontent.repository;
 
 import com.example.servingwebcontent.model.Invoice;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.time.LocalDateTime; 
 import java.util.List;
 
 public interface InvoiceRepository extends JpaRepository<Invoice, Integer> {
     List<Invoice> findByTenantId(Integer tenantId);
     List<Invoice> findByRoomId(Integer roomId);
+    List<Invoice> findByCreatedAt(LocalDateTime createdAt);
 }
