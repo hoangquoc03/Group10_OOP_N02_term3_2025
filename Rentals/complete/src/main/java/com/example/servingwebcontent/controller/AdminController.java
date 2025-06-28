514b196c9c5ee57adc9afe9d9bde7f77a399ed83
lehongquoc18 <letuan151125@gmail.com>
2025-06-28 08:15:54 +0000

    mm

---
+++ Rentals/complete/src/main/java/com/example/servingwebcontent/controller/AdminController.java
@@ -0,0 +1,20 @@
+package com.example.servingwebcontent.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/admin")
+    public String redirectToStaticAdminPage() {
+        // Chuyển hướng đến file HTML trong thư mục static
+        return "redirect:/admin/admin_dashboard.html";
+    }
+
+     @GetMapping("/")
+    public String index() {
+        // Chuyển hướng đến file HTML trong thư mục static
+        return "admin_dashboard";
+    }
+}
